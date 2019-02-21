4659a133c174e46e442a7e201a68271128121c2e
Renan Hagiwara <renan@recoha.com>
2019-02-21 17:31:43 -0300

    Adição do código para criação da base de dados.

--- unidade2-pdo/db-tables.php
+++ unidade2-pdo/db-tables.php
@@ -8,7 +8,10 @@ try {
     // Conexão com o MYSQL.
     $conexao = new PDO("mysql:dbname=exemplo; host=$endereco", $usuario, $senha);
 
-    // Executa a instrução SQL para excluir o registro com a id = 1.
+    // Executa a instrução SQL para criação da tabela.
+    $conexao->exec("CREATE DATABASE exemplo");
+
+    // Executa a instrução SQL para criação da tabela.
     $conexao->exec("CREATE TABLE usuarios (id MEDIUMINT UNSIGNED NOT NULL AUTO_INCREMENT, nome VARCHAR(80) NOT NULL, PRIMARY KEY (id, nome))");
 
     // Fecha conexão.
